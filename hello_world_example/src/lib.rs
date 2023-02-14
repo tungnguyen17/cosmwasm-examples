58becc791d841f832d066d259a97552666b9ca4f
Tung Nguyen <2715595+tungnguyen17@users.noreply.github.com>
2023-02-14 18:29:27 +0700

    hello_world: empty contract

---
+++ hello_world_example/src/lib.rs
@@ -0,0 +1,21 @@
+use cosmwasm_std::{
+  entry_point,
+  Binary,
+  Deps,
+  DepsMut,
+  Empty,
+  Env,
+  MessageInfo,
+  Response,
+  StdResult,
+};
+
+#[entry_point]
+pub fn instantiate(
+  _deps: DepsMut,
+  _env: Env,
+  _info: MessageInfo,
+  _msg: Empty,
+) -> StdResult<Response> {
+  Ok(Response::new())
+}
